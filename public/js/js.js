c356ccf601fbaddff97e867420f651134a6ac25b
Fabiano Vaz <fabiano-vaz@live.com>
2022-02-01 00:21:57 -0300

    paginação e js de função drap e drow de arquivos

---
+++ public/js/js.js
@@ -0,0 +1,17 @@
+ //call paginate
+ $('.table_aluno tbody').paginate({
+    'perPage': 5,
+    'scope': $('tr')
+  });
+  
+
+// dropify
+$(".dropify").dropify({
+  messages: {
+    default:
+      '<span class="fs-25">Arraste e solte ou clique para inserir sua foto</span>',
+    replace: "Arraste e solte ou clique para substituir",
+    remove: "Remover",
+    error: "Ooops, algo errado aconteceu.",
+  },
+});
