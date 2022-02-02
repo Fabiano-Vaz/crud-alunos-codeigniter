d29982ec7c47f0c8a748511b9bef27bdd0e7ff36
Fabiano Vaz <fabiano-vaz@live.com>
2022-02-01 21:57:45 -0300

    limite de tamanho para fotos

--- application/views/Modais/editAluno.php
+++ application/views/Modais/editAluno.php
@@ -24,7 +24,7 @@
                 <div class="row">
                   <div class="col">
                     <!-- url da foto de perfil -->
-                    <input name="foto_url" id="foto_url" accept="image/jpeg" type="file" class="dropify" data-height="100" data-allowed-file-extensions="jpg jpeg"  />
+                    <input name="foto_url" id="foto_url" accept="image/jpeg" data-max-file-size="3M" type="file" class="dropify" data-height="100" data-allowed-file-extensions="jpg jpeg"  />
                   </div>
                 </div>
               </div>
